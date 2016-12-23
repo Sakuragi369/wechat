28d5da440d2bf70b9730e8b96be2f3eb7b1ee87d
jiaojian-chris <jiaojian_chris@163.com>
2016-12-23 17:38:12 +0800

    add project

--- wechat/wsgi.py
+++ wechat/wsgi.py
@@ -11,7 +11,7 @@ import sys
 
 root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythondjangotest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wechat.settings")
 
 from django.core.wsgi import get_wsgi_application
 
